86c78dd65ee15905f6fe16fe91324ee98efa3e94
Ibrar Ramzan <ibrarramzan.dev@gmail.com>
2021-08-05 02:09:30 -0700

    MongoDB setup

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const { connectDB } = require('./db');
 const app = express();
+
+connectDB();
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'Hello world!' });
+});
 
 app.listen(process.env.PORT, () => console.log(`Server started at port ${process.env.PORT}`));
